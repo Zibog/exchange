34b14b6289be6df33801c97daa10b3c4c561a96c
Zibog <dmsidak@gmail.com>
2025-01-28 22:17:17 +0200

    Initial commit

---
+++ exchange/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+)
+
+// key for the HTTP server’s address value in the context
+const keyServerAddr = "serverAddr"
+
+func main() {
+	// server multiplexer and http.Handler implementation
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		BaseContext: func(listener net.Listener) context.Context {
+			return context.WithValue(ctx, keyServerAddr, listener.Addr().String())
+		},
+	}
+
+	serverTwo := &http.Server{
+		Addr:    ":8888",
+		Handler: mux,
+		BaseContext: func(listener net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, listener.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
+
+	// err := http.ListenAndServe(":8080", mux)
+	// if errors.Is(err, http.ErrServerClosed) {
+	// 	fmt.Printf("server closed\n")
+	// } else if err != nil {
+	// 	fmt.Printf("error starting server: %s\n", err)
+	// 	os.Exit(1)
+	// }
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n",
+		ctx.Value(keyServerAddr),
+		hasFirst, first,
+		hasSecond, second)
+
+	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "Hello, HTTP!\n")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dsidak/exchange
+
+go 1.17
